833637ded7dcc7279288fce4a9885a1e6578bec5
Pierre-Yves Rollo <pierre-yves.rollo@ign.fr>
2024-05-24 15:53:25 +0200

    perf(sqlite): Improve `save` performances by setting synchronous to off (and some code simplifications)

--- src/main/java/com/ignfab/minalac/generator/outputs/minetest/utils/SqlLiteMapWriter.java
+++ src/main/java/com/ignfab/minalac/generator/outputs/minetest/utils/SqlLiteMapWriter.java
@@ -9,33 +9,31 @@ import java.nio.file.Paths;
 import java.sql.*;
 
 public class SqlLiteMapWriter {
-    private Connection sqlFileConnector;
+    private Connection connection;
     private Serializer serializer;
 
     public SqlLiteMapWriter(String directoryFullPath) throws MapWriteException {
         if (!Files.exists(Paths.get(directoryFullPath)))
             throw new MapWriteException("The directory can not be accessed");
-        this.sqlFileConnector = createAndConnectToFileDB(directoryFullPath);
+        createAndConnectToFileDB(directoryFullPath);
         this.serializer = new Serializer();
     }
 
-    private Connection createAndConnectToFileDB(String directoryFullPath) throws MapWriteException {
-        Connection connection;
+    private void createAndConnectToFileDB(String directoryFullPath) throws MapWriteException {
         try {
             String pathDBFile = "jdbc:sqlite:" + directoryFullPath + "map.sqlite";
-            String statementTableBlock = "CREATE TABLE `blocks` (`pos` INT NOT NULL PRIMARY KEY,`data` BLOB);";
             connection = DriverManager.getConnection(pathDBFile);
-            Statement createTable = connection.createStatement();
-            createTable.execute(statementTableBlock);
+            Statement statement = connection.createStatement();
+            statement.execute("PRAGMA synchronous=OFF");
+            statement.execute("CREATE TABLE `blocks` (`pos` INT NOT NULL PRIMARY KEY,`data` BLOB)");
         } catch (SQLException e) {
             throw new MapWriteException(e.getMessage());
         }
-        return connection;
     }
 
     public void insertBlock(int pos, Block block) throws MapWriteException {
         try {
-            PreparedStatement statement = sqlFileConnector.prepareStatement("INSERT INTO blocks VALUES (?,?)");
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO blocks VALUES (?,?)");
             statement.setInt(1, pos);
             statement.setBytes(2, this.serializer.serialize(block));
             statement.execute();
