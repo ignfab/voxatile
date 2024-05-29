9cc94b12ac414ed9cee35f64d370972ce7b0b739
Pierre-Yves Rollo <dev@pyrollo.com>
2024-05-29 11:50:04 +0200

    perf(serialization): Improve Minetest serialization preformances

--- src/main/java/com/ignfab/minalac/generator/outputs/minetest/utils/Serializer.java
+++ src/main/java/com/ignfab/minalac/generator/outputs/minetest/utils/Serializer.java
@@ -5,31 +5,52 @@ import com.ignfab.minalac.generator.outputs.minetest.Block;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.BufferedOutputStream;
 import java.util.Map;
 import java.util.zip.DeflaterOutputStream;
+import java.util.zip.Deflater;
 
 public class Serializer {
 
+    // We use a static buffer to convert int32 and int64 to bytes.
+    // This avoid usage of OutputBuffer.write(int) which performs something similar
+    // but with a byte array instantiation for each call.
+    private final byte[] buffer;
+
+    // Using a static deflater avoids many useless instantiations.
+    private final Deflater deflater;
+
+    public Serializer() {
+        buffer = new byte[8192];
+        deflater = new Deflater(Deflater.BEST_COMPRESSION);
+    }
+
     private void write8Bits(OutputStream stream, int value) throws IOException {
-        stream.write((byte) (value & 0xFF));
+        buffer[0] = (byte) (value & 0xFF);
+        stream.write(buffer, 0, 1);
     }
 
     private void write16Bits(OutputStream stream, int value) throws IOException {
-        stream.write((byte) ((value >> 8) & 0xFF));
-        stream.write((byte) (value & 0xFF));
+        buffer[0] = (byte) ((value >> 8) & 0xFF);
+        buffer[1] = (byte) (value & 0xFF);
+        stream.write(buffer, 0, 2);
     }
 
     private void write32Bits(OutputStream stream, int value) throws IOException {
-        stream.write((byte) ((value >> 24) & 0xFF));
-        stream.write((byte) ((value >> 16) & 0xFF));
-        stream.write((byte) ((value >> 8) & 0xFF));
-        stream.write((byte) (value & 0xFF));
+        buffer[0] = (byte) ((value >> 24) & 0xFF);
+        buffer[1] = (byte) ((value >> 16) & 0xFF);
+        buffer[2] = (byte) ((value >> 8) & 0xFF);
+        buffer[3] = (byte) (value & 0xFF);
+        stream.write(buffer, 0, 4);
     }
 
     private void writeParam0IntoStream(OutputStream stream, Block block) throws IOException {
-        for (short param0 : block.getParam0()) {
-            write16Bits(stream, param0);
+        int i = 0;
+        for (short value : block.getParam0()) {
+            buffer[i++] = (byte) ((value >> 8) & 0xFF);
+            buffer[i++] = (byte) (value & 0xFF);
         }
+        stream.write(buffer);
     }
 
     //See World Format Documentation for more information about block serialization
@@ -57,7 +78,7 @@ public class Serializer {
     //See World Format Documentation for version 28
     //https://github.com/minetest/minetest/blob/master/doc/world_format.md
     public byte[] serialize(Block block) throws IOException {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream(16384);
 
         //u8 : map version number
         write8Bits(stream, 28);
@@ -75,20 +96,25 @@ public class Serializer {
         write8Bits(stream, 2);
 
         //Zlib Node data
-        DeflaterOutputStream nodeDataStream = new DeflaterOutputStream(stream);
+        DeflaterOutputStream nodeDataStream = new DeflaterOutputStream(stream, deflater);
+
         writeParam0IntoStream(nodeDataStream, block);
         nodeDataStream.write(block.getParam1());
         nodeDataStream.write(block.getParam2());
+
         nodeDataStream.flush();
         nodeDataStream.finish();
+        deflater.reset();
 
         //Zlib Node metadata
-        DeflaterOutputStream nodeMetadataStream = new DeflaterOutputStream(stream);
+        DeflaterOutputStream nodeMetadataStream = new DeflaterOutputStream(stream, deflater);
+
         //u8 : version
         write16Bits(nodeMetadataStream, 2);
         write16Bits(nodeMetadataStream, 0);
         nodeMetadataStream.flush();
         nodeMetadataStream.finish();
+        deflater.reset();
 
         //u8 : static object version
         write8Bits(stream, 0);
