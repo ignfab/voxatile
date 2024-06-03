22fc724e94208ee94f41fb37f3e6b9d4f54a01c0
Pierre-Yves Rollo <pierre-yves.rollo@ign.fr>
2024-06-03 16:47:02 +0200

    test(world): Add Testing output format for... testing purposes

---
+++ src/test/java/com/ignfab/minalac/generator/outputs/testing/TestingVoxelType.java
@@ -0,0 +1,53 @@
+package com.ignfab.minalac.generator.outputs.testing;
+
+import com.ignfab.minalac.generator.world.OutOfWorldException;
+import com.ignfab.minalac.generator.world.VoxelType;
+
+/**
+ * A dummuy voxelType implementation for {@code TestingVoxelWorld}.
+ *
+ * All testing voxel type are represented by simple strings.
+ */
+public class TestingVoxelType implements VoxelType {
+    /**
+     * World where voxel are placed.
+     */
+    protected TestingVoxelWorld world;
+
+    /**
+     * Type of the voxel type.
+     */
+    protected String type;
+
+    /**
+     * Creates a new TestingVoxelType.
+     *
+     * @param world World in which this voxel type will be placed
+     * @param type A string for this voxel type (this value will be set in voxel world)
+     */
+    protected TestingVoxelType(TestingVoxelWorld world, String type) {
+        this.world = world;
+        this.type = type;
+    }
+
+    /**
+     * Places a voxel of this type in the world.
+     *
+     * @param x x-coordinate where to place voxel
+     * @param y y-coordinate where to place voxel
+     * @param z z-coordinate where to place voxel
+     */
+    @Override
+    public void place(int x, int y, int z) throws OutOfWorldException {
+        this.world.set(x, y, z, this);
+    }
+
+    /**
+     * Returns the string type of that voxel type.
+     *
+     * @return Voxel type as string
+     */
+    protected String getType() {
+        return type;
+    }
+}
