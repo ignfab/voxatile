0a09c23fd3df27635058cb1f5a4231b45d4c14e9
Pierre-Yves Rollo <dev@pyrollo.com>
2024-06-21 11:44:05 +0200

    feat(models): Use bbox intersection in GeometryModel

--- src/main/java/com/ignfab/minalac/generator/utils/world2d/chunk/IterableChunk2d.java
+++ src/main/java/com/ignfab/minalac/generator/utils/world2d/chunk/IterableChunk2d.java
@@ -1,7 +1,8 @@
 package com.ignfab.minalac.generator.utils.world2d.chunk;
 
+import java.util.Iterator;
+
 import com.ignfab.minalac.generator.utils.world2d.iterator.Chunk2dElement;
-import com.ignfab.minalac.generator.utils.world2d.iterator.Chunk2dIterator;
 
 /**
  * The {@code IterableChunk2d} interface represents an iterable {@link ReadableChunk2d}.
@@ -12,5 +13,5 @@ public interface IterableChunk2d extends ReadableChunk2d, Iterable<Chunk2dElemen
      *
      * @return a {@link com.ignfab.minalac.generator.utils.world2d.iterator.Chunk2dIterator} to iterate over the elements.
      */
-    Chunk2dIterator iterator();
+    Iterator<Chunk2dElement> iterator();
 }
