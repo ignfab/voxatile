5065d7963c37a02cae879694e4b07b87b86c235d
Pierre-Yves Rollo <dev@pyrollo.com>
2024-09-09 11:57:37 +0200

    feat(iterators): Add generic iterators
    Co-authored-by: indyteo <theo.mail@laposte.net>

---
+++ src/main/java/com/ignfab/minalac/generator/utils/iterator/Iterables.java
@@ -0,0 +1,115 @@
+package com.ignfab.minalac.generator.utils.iterator;
+
+import java.util.Iterator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * An utility class for operations on iterables.
+ */
+public final class Iterables {
+    private Iterables() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Creates an iterable oven given array.
+     *
+     * @param array an array of items
+     *
+     * @return an iterable over items of provided array
+     *
+     * @param <T> type of items
+     */
+    public static <T> Iterable<T> array(T[] array) {
+        return () -> Iterators.array(array);
+    }
+
+    /**
+     * Filters result of an iterable according to a condition.
+     * Only elements for which condition is true are returned.
+     *
+     * @param iterable iterable to filter
+     * @param condition predicate telling which results to keep
+     *
+     * @return an iterable returning only results for which condition is true
+     *
+     * @param <T> type returned by iterable
+     */
+    public static <T> Iterable<T> filter(Iterable<T> iterable, Predicate<T> condition) {
+        return () -> Iterators.filter(iterable.iterator(), condition);
+    }
+
+    /**
+     * Creates an iterable oven all items given as argument.
+     * Using this method with only one argument is the right way to create a singleton iterable.
+     *
+     * @param items items iterable should return
+     *
+     * @return an iterable over items given as arguments
+     *
+     * @param <T> type of items
+     */
+    @SafeVarargs
+    public static <T> Iterable<T> iterable(T... items) {
+        return array(items);
+    }
+
+    /**
+     * Remap an iterable, transforming its result into another value using a
+     * provided function.
+     *
+     * @param iterable the iterable to remap
+     * @param mapper the mapping function
+     *
+     * @return an iterable returning transformed results
+     *
+     * @param <T> original type returned by iterable
+     * @param <U> new type returned by resulting iterable
+     */
+    public static <T, U> Iterable<U> remap(Iterable<T> iterable, Function<T, U> mapper) {
+        return () -> Iterators.remap(iterable.iterator(), mapper);
+    }
+
+    /**
+     * Merges several iterables into one.
+     *
+     * @param iterables iterables to merge
+     *
+     * @return an iterable returning all results of iterables
+     *
+     * @param <T> common type returned by iterables to merge
+     */
+    @SafeVarargs
+    public static <T> Iterable<T> union(Iterable<? extends T>... iterables) {
+        return () -> Iterators.unwrap(Iterators.remap(Iterators.array(iterables), Iterable::iterator));
+    }
+
+     /**
+     * Unwraps an iterable over iterables into an iterable over iterables results.
+     *
+     * @param iterable iterable over iterables
+     *
+     * @return an iterable returning all results of iterable iterables
+     *
+     * @param <T> common type returned by iterable iterables
+     */
+    public static <T> Iterable<T> unwrap(Iterable<? extends Iterable<? extends T>> iterable) {
+        return () -> Iterators.unwrap(Iterators.remap(iterable.iterator(), Iterable::iterator));
+    }
+
+    /**
+     * Unwraps an iterable over iterators into an iterable over iterators results.
+     *
+     * @param iterable iterable over iterators
+     *
+     * @return an iterable returning all results of iterable iterators
+     *
+     * @param <T> common type returned by iterables iterators
+     */
+    public static <T> Iterable<T> unwrapIterators(Iterable<? extends Iterator<? extends T>> iterable) {
+        return () -> Iterators.unwrap(iterable.iterator());
+    }
+
+}
+
---
+++ src/test/java/com/ignfab/minalac/generator/utils/iterator/IteratorTesterTest.java
@@ -0,0 +1,165 @@
+package com.ignfab.minalac.generator.utils.iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+
+public class IteratorTesterTest {
+
+    @Test
+    void testAssertBrowseAllOnce() {
+        List<String> expected = Arrays.asList("a", "c", "b");
+        List<String> iterable = Arrays.asList("a", "b", "c");
+
+        // Should be the same in different orders, iterable or iterator
+        assertDoesNotThrow(() -> {
+            IteratorTester.assertBrowsesAllOnce(expected, iterable.iterator());
+        });
+
+        assertDoesNotThrow(() -> {
+            IteratorTester.assertBrowsesAllOnce(expected, iterable);
+        });
+
+        // Missing with distinct values
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Arrays.asList(3, 4, 5, 6),
+                Arrays.asList(6, 5, 3));
+        });
+
+        // Missing with same values
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Arrays.asList(3, 3, 5, 6),
+                Arrays.asList(6, 5, 3));
+        });
+
+        // Unexpected with distinct values
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Arrays.asList(1.1, 2.2, 3.3),
+                Arrays.asList(3.3, 2.2, 2.1, 1.1));
+        });
+
+        // Unexpected with same values
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Arrays.asList(1.1, 2.2, 3.3),
+                Arrays.asList(1.1, 2.2, 3.3, 3.3));
+        });
+
+        // Got something different but with the same count
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Arrays.asList("X", "Y", "Z", "T"),
+                Arrays.asList("X", "Z", "Y", "U"));
+        });
+
+        // Same count, same values, but different
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Arrays.asList("X", "Y", "Z", "Z"),
+                Arrays.asList("X", "Z", "Y", "Y"));
+        });
+
+        // Test empty stuff
+        assertDoesNotThrow(() -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Collections.emptyList(),
+                Collections.emptyIterator());
+        });
+
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Arrays.asList("Yolo"),
+                Collections.emptyIterator());
+        });
+
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAllOnce(
+                Collections.emptyList(),
+                Arrays.asList("Bruh"));
+        });
+    }
+
+    @Test
+    void testAssertBrowseAll() {
+        List<String> expected = Arrays.asList("a", "c", "b");
+        List<String> iterable = Arrays.asList("a", "b", "c");
+
+        // Should be the same in different orders, iterable or iterator
+        assertDoesNotThrow(() -> {
+            IteratorTester.assertBrowsesAll(expected, iterable.iterator());
+        });
+
+        assertDoesNotThrow(() -> {
+            IteratorTester.assertBrowsesAll(expected, iterable);
+        });
+
+        // Missing with distinct values
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAll(
+                Arrays.asList(3, 4, 5, 6),
+                Arrays.asList(6, 5, 3));
+        });
+
+        // Missing with same values
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAll(
+                Arrays.asList(3, 3, 5, 6),
+                Arrays.asList(6, 5, 3));
+        });
+
+        // Unexpected with distinct values
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAll(
+                Arrays.asList(1.1, 2.2, 3.3),
+                Arrays.asList(3.3, 2.2, 2.1, 1.1));
+        });
+
+        // Browsing twice the same value
+        assertDoesNotThrow(() -> {
+            IteratorTester.assertBrowsesAll(
+                Arrays.asList(1.1, 2.2, 3.3),
+                Arrays.asList(1.1, 2.2, 3.3, 3.3));
+        });
+
+        // Got something different but with the same count
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAll(
+                Arrays.asList("X", "Y", "Z", "T"),
+                Arrays.asList("X", "Z", "Y", "U"));
+        });
+
+        // Expected twice the same item
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAll(
+                Arrays.asList("X", "Y", "Z", "Z"),
+                Arrays.asList("X", "Z", "Y", "Y"));
+        });
+
+        // Test empty stuff
+        assertDoesNotThrow(() -> {
+            IteratorTester.assertBrowsesAll(
+                Collections.emptyList(),
+                Collections.emptyIterator());
+        });
+
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAll(
+                Arrays.asList("Yolo"),
+                Collections.emptyIterator());
+        });
+
+        assertThrows(AssertionFailedError.class, () -> {
+            IteratorTester.assertBrowsesAll(
+                Collections.emptyList(),
+                Arrays.asList("Bruh"));
+        });
+    }
+}
