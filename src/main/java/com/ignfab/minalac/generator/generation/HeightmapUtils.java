8658ae1deb0701df6fa3792d136bc59ed90702bc
zhak5388 <102757175+zhak5388@users.noreply.github.com>
2024-11-06 17:31:21 +0100

    (wip__experimentation): begin manhattan algorithm [TODO: testing perfs, if ok, standards todos]

--- src/main/java/com/ignfab/minalac/generator/generation/HeightmapUtils.java
+++ src/main/java/com/ignfab/minalac/generator/generation/HeightmapUtils.java
@@ -40,4 +40,53 @@ public final class HeightmapUtils {
         return (int) Math.floor(mean / (kernelSize * kernelSize));
     }
 
+    // TODO-PR : Experimentation
+
+    // Can be improved if applied globally
+    // if 0 => set(1)
+    // (if not0 and not default => min(around) + 1)
+    public static void applyManhattan(Heightmap heightmap) {
+        WorldBBox2d bbox = heightmap.bbox();
+        for (int x = bbox.minX(); x <= bbox.maxX(); x++)
+            for (int y = bbox.minY(); y <= bbox.maxY(); y++)
+                if (heightmap.get(x, y) > 0)
+                    manhattan(heightmap, x, y);
+    }
+
+    public static void manhattan(Heightmap heightmap, int x, int y) {
+        int k = 0;
+        boolean foundZero = false;
+        // max k is arbitrary it depends on ReadableHM or WritableHM
+        while (!foundZero && k <= 50) {
+            k++;
+            foundZero = around(heightmap, x, y, k);
+        }
+        if (foundZero)
+            heightmap.set(x, y, k);
+    }
+
+    public static boolean around(Heightmap heightmap, int x, int y, int k) {
+        int i = -k;
+        int x1, y1, x2, y2, x3, y3, x4, y4;
+
+        // Not really a manhattan (diag = 1 and not 2)
+        while(i <= k - 1) {
+            x1 = i + 1;
+            y1 = k;
+            x2 = k;
+            y2 = i;
+            x3 = i;
+            y3 = -k;
+            x4 = -k;
+            y4 = i + 1;
+            if (heightmap.get(x + x1, y + y1) == 0 ||
+                heightmap.get(x + x2, y + y2) == 0 ||
+                heightmap.get(x + x3, y + y3) == 0 ||
+                heightmap.get(x + x4, y + y4) == 0)
+                return true;
+            i++;
+        }
+        return false;
+    }
+
 }
