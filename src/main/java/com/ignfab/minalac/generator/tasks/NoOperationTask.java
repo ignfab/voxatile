113bf55c983f7dc676b37b461468407c8ad1f8ff
Pierre-Yves Rollo <dev@pyrollo.com>
2025-09-29 15:26:27 +0200

    feat(noop): Add a NoOperation task for organisational purpose

---
+++ src/main/java/com/ignfab/minalac/generator/tasks/NoOperationTask.java
@@ -0,0 +1,23 @@
+package com.ignfab.minalac.generator.tasks;
+
+import com.ignfab.minalac.generator.generation.GenerationTile;
+
+/**
+ * A task doing absolutely nothing (because nothing is important).
+ * <p>
+ * Very convenient to wait for other tasks and use same abstract name in subsequent {@code after} fields.
+ */
+public final class NoOperationTask implements TileTask {
+
+    /**
+     * This class is a singleton, use this instead of constructor.
+     */
+    public static final NoOperationTask INSTANCE = new NoOperationTask();
+
+    private NoOperationTask() {
+    }
+
+    @Override
+    public void run(GenerationTile tile) {
+    }
+}
