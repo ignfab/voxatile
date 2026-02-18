c837cca3ee44c0ea2f088049a2d33c2490d75634
zhak5388 <102757175+zhak5388@users.noreply.github.com>
2026-02-18 18:05:32 +0100

    wip - delegate recur

--- src/main/java/com/ignfab/minalac/generator/placeables/resized/builders/SuperDelegateIndexMapperBuilder.java
+++ src/main/java/com/ignfab/minalac/generator/placeables/resized/builders/SuperDelegateIndexMapperBuilder.java
@@ -1,14 +1,27 @@
 package com.ignfab.minalac.generator.placeables.resized.builders;
 
+import java.util.Arrays;
+
 import com.ignfab.minalac.generator.placeables.resized.IndexMapper;
 import com.ignfab.minalac.generator.placeables.resized.IndexMapperBuilder;
 import com.ignfab.minalac.generator.placeables.resized.mappers.IdentityIndexMapper;
 
 public class SuperDelegateIndexMapperBuilder implements IndexMapperBuilder {
-    IndexMapperBuilder[] tab;
+    private final IndexMapperBuilder[] builders;
+    private final int minSize;
 
-    public SuperDelegateIndexMapperBuilder(IndexMapperBuilder[] tab) {
-        this.tab = tab;
+    public SuperDelegateIndexMapperBuilder(IndexMapperBuilder[] builders) {
+        this.builders = builders;
+        // TODO-14 : Il y a d'autre comptabilité mais c'est le plus bourrin
+        int maxMin = 0;
+        for (IndexMapperBuilder builder : builders)
+            maxMin = Math.max(maxMin, builder.minimumSize());
+
+        for (IndexMapperBuilder builder : builders) {
+            if (builder.maxSizeUnder(maxMin) != maxMin)
+                throw new RuntimeException("Incompatible builders");
+        }
+        this.minSize = maxMin;
     }
 
     @Override
@@ -22,21 +35,38 @@ public class SuperDelegateIndexMapperBuilder implements IndexMapperBuilder {
 
     @Override
     public int maxSizeUnder(int size) {
-        for (IndexMapperBuilder builder : tab) {
-            if (builder.maxSizeUnder(size) != size)
+        /*
+        int minCandidate = Integer.MAX_VALUE;
+        for (IndexMapperBuilder builder : builders) {
+            int currentMax = builder.maxSizeUnder(size);
+            minCandidate = Math.min(minCandidate, currentMax);
+            if (currentMax != size)
                 return -1;
         }
         return size;
+         */
+        return compute(size);
+    }
+
+    // TODO-15 : Ca me semble OK mais vérifier
+    private int compute(int size) {
+        if (size < 0)
+            return size;
+        boolean disagree = false;
+        int minCandidate = size;
+        for (IndexMapperBuilder builder : builders) {
+            int currentPossibleValue = builder.maxSizeUnder(size);
+            minCandidate = Math.min(minCandidate, currentPossibleValue);
+            if (currentPossibleValue != size)
+                disagree = true;
+        }
+        if (disagree)
+            return compute(minCandidate);
+        return minCandidate;
     }
 
     @Override
     public int minimumSize() {
-        int min = tab[0].minimumSize();
-        for (int i = 1; i < tab.length; i++) {
-            if (min > tab[i].minimumSize()) {
-                min = tab[i].minimumSize();
-            }
-        }
-        return min;
+        return minSize;
     }
 }
