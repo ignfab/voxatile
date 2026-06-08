8aa09d6891516fc79f3bd97bf9a650c6636e5aae
Pierre-Yves Rollo <dev@pyrollo.com>
2026-06-08 11:23:07 +0200

    refactor(luanti): Rename MT to Luanti

--- src/main/java/com/ignfab/minalac/generator/modules/luanti/LuantiVoxel.java
+++ src/main/java/com/ignfab/minalac/generator/modules/luanti/LuantiVoxel.java
@@ -1,4 +1,4 @@
-package com.ignfab.minalac.generator.modules.minetest;
+package com.ignfab.minalac.generator.modules.luanti;
 
 import java.util.Objects;
 
@@ -6,20 +6,19 @@ import com.ignfab.minalac.generator.placeables.Placeable;
 import com.ignfab.minalac.generator.world.VoxelTile;
 
 /**
- * {@code MTVoxel} class implements a {@link Placeable} voxel for Minetest.
- * A voxel in Minetest, known as node, consists of three parameters: type, param1, param2.
- * @see <a href="https://github.com/minetest/minetest/blob/master/src/mapnode.h#L138">Minetest's MapNode class</a> for more information about the node's parameters
+ * {@code LuantiVoxel} class implements a {@link Placeable} voxel for Luanti.
+ * A voxel in Luanti, known as node, consists of three parameters: type, param1, param2.
+ * @see <a href="https://github.com/luanti-org/luanti/blob/master/src/mapnode.h#L138">Luanti's MapNode class</a> for more information about the node's parameters
  */
-public class MTVoxel implements Placeable {
+public class LuantiVoxel implements Placeable {
 
     /**
      * Default voxel used on map initialization.
      */
-    public static final MTVoxel DEFAULT_VOXEL = new MTVoxel("air", (byte) 0, (byte) 0);
+    public static final LuantiVoxel DEFAULT_VOXEL = new LuantiVoxel("air", (byte) 0, (byte) 0);
 
     /**
      * The node type string.
-     * @see <a href="https://wiki.minetest.net/Games/Minetest_Game/Nodes">List of node types (Minetest Wiki)</a>
      */
     protected String type;
     /**
@@ -34,14 +33,14 @@ public class MTVoxel implements Placeable {
     protected byte param2;
 
     /**
-     * Constructs a new {@code MTVoxel}.
+     * Constructs a new {@code LuantiVoxel}.
      *
      * @param type the node type string
      * @param param1 the param1 data of the node
      * @param param2 the param2 data of the node
-     * @see <a href="https://github.com/minetest/minetest/blob/master/src/mapnode.h#L138">Minetest's MapNode class</a> for more information about the node's parameters
+     * @see <a href="https://github.com/luanti-org/luanti/blob/master/src/mapnode.h#L138">Luanti's MapNode class</a> for more information about the node's parameters
      */
-    public MTVoxel(String type, byte param1, byte param2) {
+    public LuantiVoxel(String type, byte param1, byte param2) {
         this.type = type;
         this.param1 = param1;
         this.param2 = param2;
@@ -49,23 +48,23 @@ public class MTVoxel implements Placeable {
 
     @Override
     public void place(VoxelTile tile, int x, int y, int z)  {
-        if (tile instanceof MTVoxelTile mtTile) {
-            place(mtTile, x, y, z);
+        if (tile instanceof LuantiVoxelTile luantiTile) {
+            place(luantiTile, x, y, z);
         } else {
             throw new IllegalArgumentException("Voxel does not match voxel tile output format");
         }
     }
 
     /**
-     * Places this voxel on a {@link MTVoxelTile} at given position.
+     * Places this voxel on a {@link LuantiVoxelTile} at given position.
      *
      * @param tile tile to place into
      * @param x position x-coordinate
      * @param y position y-coordinate
      * @param z position z-coordinate
      */
-    private void place(MTVoxelTile tile, int x, int y, int z)  {
-        // The y-axis in Minetest corresponds, in our chosen coordinate system, to the z-axis, hence the inversion
+    private void place(LuantiVoxelTile tile, int x, int y, int z)  {
+        // The y-axis in Luanti corresponds, in our chosen coordinate system, to the z-axis, hence the inversion
         tile.set(x, z, y, this);
     }
 
@@ -96,7 +95,7 @@ public class MTVoxel implements Placeable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        MTVoxel that = (MTVoxel) o;
+        LuantiVoxel that = (LuantiVoxel) o;
         return param1 == that.param1 && param2 == that.param2 && type.equals(that.type);
     }
 
