4305481aaa0ee59b904a1d551c0c193ae1e931a9
indyteo <theo.mail@laposte.net>
2026-07-02 14:21:09 +0200

    model store CME fix

--- src/main/java/com/ignfab/minalac/generator/models/ModelStore.java
+++ src/main/java/com/ignfab/minalac/generator/models/ModelStore.java
@@ -19,7 +19,7 @@ public class ModelStore {
      * @param type Type to associate with stored models
      * @param models Models to be stored
      */
-    public void add(String type, List<Model> models) {
+    public synchronized void add(String type, List<Model> models) {
         if (type == null || type.isEmpty())
             throw new IllegalArgumentException("Type must be a non empty string");
 
